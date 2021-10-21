b3ff4023d5e2e0f1ca78230eea9ccb8d624694bd
LilyCrown999 <adexadexdede@gmail.com>
2021-10-21 20:39:58 +0100

    Trying to fix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 
 import express  from 'express'
 import exphbs from 'express-handlebars'
-import methodOverride from "method-override";
 import { connectDB } from './db'
 import * as alias from './Controllers/aliasController'
 import favicon from 'serve-favicon'
@@ -16,19 +15,19 @@ const app: any = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
 
+
+app.get('/', (req : any, res: any) => res.redirect('/home'))
+
+const gh : any = app.get('/home', alias.GetHome)
+app.get('/urls', alias.GetUrl);
+app.post('/create', alias.PostUrl);
+app.get('/:alias', alias.GetUrlALias)
+// app.put('/:alias', alias.PutUrlAlias)
+// app.delete('/:alias', alias.DeleteUrlAlias)
+
 app.set("port", process.env.PORT || 5500);
 app.use(favicon(path.join(__dirname, '../public','images', 'favicon.ico')))
 
-
-//  Method override
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      let method = req.body._method
-      delete req.body._method
-      return method
-    }
-  }))
 
   // Hamdlebars helpers
 import {
@@ -48,14 +47,6 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs');
 
-app.get('/', (req : any, res: any) => res.redirect('/home'))
-
-app.get('/home', alias.GetHome)
-app.get('/urls', alias.GetUrl);
-app.post('/', alias.PostUrl);
-app.get('/:alias', alias.GetUrlALias)
-// app.put('/:alias', alias.PutUrlAlias)
-// app.delete('/:alias', alias.DeleteUrlAlias)
 
 
  const PORT : any = app.get('port');
