43fe10fd53621893e812335442822b0d1f3bb201
LilyCrown999 <adexadexdede@gmail.com>
2021-11-24 18:36:09 +0100

    Cors Error Fix

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ connectDB()
 
 const app: any = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 // Our Express APP config
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
